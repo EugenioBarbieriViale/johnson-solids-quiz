bed26aaa153ed986efcfde564a3e83c1cca31a59
EugenioBarbieriViale <eugenio.barbieri.viale@gmail.com>
2025-06-21 19:07:21 +0200

    new

--- dataloader.py
+++ dataloader.py
@@ -4,22 +4,19 @@ from pandas import read_csv
 from cv2 import imread, resize, INTER_AREA
 
 import torch
-import torchvision.transforms
+from torchvision.transforms import Compose, ToTensor, Normalize
+
 
 class JS_Dataset(torch.utils.data.Dataset):
     def __init__(self, data_path="data/images", labels_path="data/names.csv"):
         super().__init__()
 
-        transform = transforms.Compose([
-            transforms.ToTensor(),
-            transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
-        ])
-
         imgs = []
-        for root, sub, file in os.walk(path):
+        for root, sub, file in os.walk(data_path):
             for f in file:
                 filepath = os.path.join(root, f)
-                img = imread(filepath, 0) # 0 is grayscale
+                # img = imread(filepath, 0) # 0 is grayscale
+                img = imread(filepath)
 
                 img = resize(img, (64, 64), interpolation=INTER_AREA)
                 imgs.append(img)
@@ -28,10 +25,15 @@ class JS_Dataset(torch.utils.data.Dataset):
         self.labels = read_csv(labels_path).values.flatten()
 
     def __getitem__(self, index):
+        transform = Compose([
+            ToTensor(),
+            Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+        ])
+
         x = self.dataset[index]
         y = self.labels[index]
 
-        return transform(x), transform(y)
+        return transform(x), y 
     
     def __len__(self):
         return len(self.dataset)
